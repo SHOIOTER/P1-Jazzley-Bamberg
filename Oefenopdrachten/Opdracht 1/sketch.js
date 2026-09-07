274e82df1857f58583933db068e9f1bd82a6270a
SHOIOTER <jazzleybee@gmail.com>
2026-09-07 20:18:18 +0200

    More progress
    progress updates

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -5,6 +5,9 @@ let chessboardPixelList = [
 ];
 let chessboardPixelSize = 25;
 
+let trafficLightFrameHeight = 140;
+let trafficLightDiameter = 30;
+
 let marioPixelList = [
   [-1, -1, -1, 0, 0, 0, 0, 0],
   [-1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -50,6 +53,7 @@ function draw() {
   translate(20, 25);
 
   text("1:", 0, 0);
+
   textSize(10);
   fill(0, 100, 0);
   text("Jazzley Bamberg", 15, 0);
@@ -61,6 +65,7 @@ function draw() {
   translate(20, 60);
 
   text("2:", 0, 0);
+
   noStroke();
   fill(255, 0, 0);
   rect(15, 0, 100, 20);
@@ -81,14 +86,56 @@ function draw() {
   ];
 
   text("3:", 0, 0)
+
+  noFill();
+  strokeWeight(5);
+  square(15, 0, chessboardPixelSize * 3);
+
   noStroke();
   drawPixels(chessboardPixelList, chessboardPixelSize, chessboardPixelColors, 15, 0);
 
   pop();
 
+  // Transparent house
+  push();
+  translate(20, 290);
+
+  text("4:", 0, 0);
+
+  noFill();
+  strokeWeight(5);
+  triangle(20, 0, 60, -35, 100, 0);
+  square(20, 0, 80);
+
+  pop();
+
+  // Traffic light
+  push();
+  translate(150, 25);
+
+  text("5:", 0, 0);
+
+  noStroke();
+  fill(150, 150, 150);
+  rect(25, 0, 40, trafficLightFrameHeight);
+  rect(35, trafficLightFrameHeight, 20, 40);
+
+  let trafficLightRadius = trafficLightDiameter / 2;
+
+  fill(255, 0, 0);
+  circle(45, trafficLightRadius + 10, trafficLightDiameter);
+
+  fill(255, 155, 0);
+  circle(45, 70, trafficLightDiameter);
+
+  fill(0, 255, 0);
+  circle(45, trafficLightFrameHeight - trafficLightRadius - 10, trafficLightDiameter);
+
+  pop();
+
   // Pixel Mario
   push();
-  translate(275, 250);
+  translate(350, 35);
 
   let marioPixelColors = [
     color(255, 0, 0),
@@ -96,8 +143,12 @@ function draw() {
     color(155, 155, 0)
   ];
 
+  text("7:", 0, 0);
+
+  text("Mario", 30, -10);
+
   noStroke();
-  drawPixels(marioPixelList, marioPixelSize, marioPixelColors, 0, 0);
+  drawPixels(marioPixelList, marioPixelSize, marioPixelColors, 15, 0);
 
   pop();
 }
