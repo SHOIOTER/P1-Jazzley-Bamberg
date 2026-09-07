f742ec582350e27953aa052bc0dba32fee0347aa
SHOIOTER <jazzleybee@gmail.com>
2026-09-07 16:32:37 +0200

    Added p5.js
    Some changes have been made, test.

--- Oefenopdrachten/Opdracht 1/sketch.js
+++ Oefenopdrachten/Opdracht 1/sketch.js
@@ -1,7 +1,25 @@
+let percentage = 0;
+let duration = 60;
+
+let ballRadius = 20;
+
+let startLevel = 200;
+let floorLevel = ballRadius;
+let difference = startLevel - floorLevel;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(255, 0, 0);
+  
+  percentage = (frameCount % duration) / duration;
+  
+  fill(200, 200, 200)
+  circle(
+    width / 2,
+    startLevel + difference * pow(percentage * 2 - 1, 2),
+    ballRadius * 2
+  );
 }
