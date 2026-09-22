48dbb633984cdf3d7a492dd166dd36beefe672a2
SHOIOTER <jazzleybee@gmail.com>
2026-09-22 15:57:45 +0200

    Progress

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -6,7 +6,9 @@ let boardSize = windowSize * boardScale;
 let boardOffset = boardSize / 2;
 let boardPos = windowOffset - boardOffset;
 
-let gridSize = 4;
+// This controls the side of the board, a bigger number means more buttons
+
+let gridSize = 3;
 let gridStop = gridSize - 1;
 let gridList = [];
 for (let y = 0; y < gridSize; y++) {
@@ -26,6 +28,8 @@ let buttonColors;
 let buttonHoverColor;
 let buttonNoLineColor;
 
+// This controls how many players there are
+
 let playerAmount = 2;
 let currentPlayerID = 1;
 
@@ -41,7 +45,16 @@ let textMaxRotation;
 let textRotateSpeed = 0.04;
 
 let turnTextPosY = windowSize * (1 - boardScale) / 4;
+let turnTextBounceStart = turnTextPosY * 2;
+let turnTextBounceDuration = 60;
 let turnText;
+
+/*
+   These are the names of the players, if a name is missing it defaults to "undefined"
+   That's actually unintentional
+   But when adding undefined to a string, it actually says for example: "It's undefined's turn"
+*/
+
 let playerNames = [
   "Blue",
   "Red"
@@ -54,11 +67,30 @@ let resetTextColliderWidth = 400;
 let resetTextColliderHeight = 50;
 let resetTextColliderX = textPosX - resetTextColliderWidth / 2;
 let resetTextColliderY = resetTextPosY - resetTextColliderHeight / 2;
+let resetTextFloatAmplitude = turnTextPosY / 3;
+let resetTextFloatDuration = 90;
 
 let roundOver = false;
 let roundOverText;
 
 let checkWinConditionFunctions = [];
+
+let clickSound;
+let drawSound;
+let winSounds;
+let restartSound;
+
+// Initializing some variables in the setup function
+
+function preload() {
+  clickSound = loadSound("./Assets/Button_Click.mp3");
+  drawSound = loadSound("./Assets/Draw_Sound.mp3");
+  winSounds = [
+    loadSound("./Assets/Green_Giant.mp3"),
+    loadSound("./Assets/Heheheha.mp3")
+  ];
+  restartSound = loadSound("./Assets/Green_Giant.mp3");
+}
 
 function setup() {
   buttonColors = [
@@ -73,17 +105,25 @@ function setup() {
 
   textMaxRotation = radians(6);
 
-  turnTextColor = color(200);
+  turnTextColor = color(0, 200, 255);
 
-  resetTextColor = color(255, 155, 0);
-  resetTextHoverColor = color(100);
+  resetTextColor = color(255, 200, 0);
+  resetTextHoverColor = color(150);
 
   createCanvas(windowSize, windowSize);
 }
 
+// Calculates the spacing for the button, used to place every specific button
+
 function getButtonSpacing(a) {
   return buttonSpacing * lerp(-buttonsOnSide, buttonsOnSide, a);
 }
+
+/*
+   A function that calls the callback function for every button
+   The callback has some parameters
+   The x and y position of the button, the rowValues and the x value
+*/
 
 function forEachButton(callback) {
   for (let y = 0; y < gridSize; y++) {
@@ -99,12 +139,16 @@ function forEachButton(callback) {
   }
 }
 
+// Returns true if the mouse is inside of the button's bounds
+
 function mouseInBounds(buttonPosX, buttonPosY, sizeX, sizeY) {
-  return mouseX >= buttonPosX
-    && mouseX <= buttonPosX + (sizeX || buttonSize)
-    && mouseY >= buttonPosY
-    && mouseY <= buttonPosY + (sizeY || buttonSize);
+  return mouseX > buttonPosX
+    && mouseX < buttonPosX + (sizeX || buttonSize)
+    && mouseY > buttonPosY
+    && mouseY < buttonPosY + (sizeY || buttonSize);
 }
+
+// Displays the buttons on the board, also making sure they get the right color
 
 function drawButtons(buttonPosX, buttonPosY, rowValues, x) {
   let colorValue = rowValues[x];
@@ -121,18 +165,36 @@ function drawButtons(buttonPosX, buttonPosY, rowValues, x) {
   );
 }
 
+/*
+   Changes the current player to something else
+   Also resets the backgroundlerping values, so it will lerp towards the new player color
+*/
+
 function changeCurrentPlayer(newCurrentPlayerID) {
   backgroundTransitionStart = buttonColors[currentPlayerID] || buttonNoLineColor;
   backgroundTransitionElapsed = 0;
   currentPlayerID = newCurrentPlayerID;
 }
 
+/*
+   Checks if any buttons are clicked, will be called inside the mouseClicked function
+   If that's the case, it will place the right value into the right column inside the row
+   Which will make it clear which player clicked that button
+*/
+
 function clickButtons(buttonPosX, buttonPosY, rowValues, x) {
   if (rowValues[x] == 0 && mouseInBounds(buttonPosX, buttonPosY)) {
+    clickSound.play();
     rowValues[x] = currentPlayerID;
-    changeCurrentPlayer(currentPlayerID % playerAmount + 1)
+    changeCurrentPlayer(currentPlayerID % playerAmount + 1);
   }
 }
+
+/*
+   This checks if the board is full
+   This is done after checking if there are lines formed
+   This will always result in a draw if board is full and no lines were formed
+*/
 
 function boardIsFull() {
   let boardFull = true;
@@ -152,15 +214,36 @@ function boardIsFull() {
   return boardFull;
 }
 
-function endRound(endText) {
+/*
+   This makes the round end, will also asign the endText to the roundOverText
+   Which will make it that the top text will display something else besides who's turn it is
+*/
+
+function endRound(endText, noLineGridList) {
+  noLineGridList = noLineGridList || []
+
   for (let y = 0; y < gridSize; y++) {
     let rowValues = gridList[y];
+    let noLineRowValues = noLineGridList[y] || [];
+
     for (let x = 0; x < gridSize; x++) {
-      rowValues[x] = -1;
+      if (!noLineRowValues[x]) {
+        rowValues[x] = -1;
+      }
     }
   }
+
   roundOverText = endText;
   roundOver = true;
+  (winSounds[currentPlayerID - 1] || drawSound).play();
+}
+
+function getQuadBounceAlpha(t) {
+  return -pow(t * 2 - 1, 2) + 1;
+}
+
+function getQuadFloatAmplitude(t) {
+  return abs(t * 2 - 1) * 2 - 1;
 }
 
 function draw() {
@@ -186,7 +269,7 @@ function draw() {
   forEachButton(drawButtons);
 
   push();
-  translate(textPosX, turnTextPosY);
+  translate(textPosX, lerp(turnTextBounceStart, turnTextPosY, getQuadBounceAlpha(frameCount % turnTextBounceDuration / turnTextBounceDuration)));
 
   let textScaleAlpha = frameCount * textScaleSpeed;
   let textRotateAlpha = frameCount * textRotateSpeed;
@@ -205,9 +288,14 @@ function draw() {
 
   pop();
 
+  // If the round is over, the reset button will appear
+
   if (roundOver) {
     push();
-    translate(textPosX, resetTextPosY);
+    translate(
+      textPosX,
+      resetTextPosY + getQuadFloatAmplitude(frameCount % resetTextFloatDuration / resetTextFloatDuration) * resetTextFloatAmplitude
+    );
 
     stroke(0);
     strokeWeight(5);
@@ -233,6 +321,11 @@ function draw() {
 function mouseClicked() {
   forEachButton(clickButtons);
 
+  /*
+     If the round is over it will check if the reset button is clicked
+     Else it will check if a line win condition has formed
+  */
+
   if (roundOver) {
     if (mouseInBounds(
       resetTextColliderX,
@@ -247,13 +340,17 @@ function mouseClicked() {
       }
       changeCurrentPlayer(1);
       roundOver = false;
+      restartSound.play();
     }
   } else {
     for (let checkWinCondition of checkWinConditionFunctions) {
-      let winningPlayerID = checkWinCondition();
+      let [winningPlayerID, noLineGridList] = checkWinCondition();
       if (winningPlayerID) {
         currentPlayerID = winningPlayerID;
-        endRound(playerNames[winningPlayerID - 1] + " has won!");
+        endRound(
+          playerNames[winningPlayerID - 1] + " has won!",
+          noLineGridList
+        );
         return;
       }
     }
@@ -268,43 +365,64 @@ function mouseClicked() {
 
 checkWinConditionFunctions.push(function () {
   let winningPlayerID;
+  let noLineGridList = [];
 
   for (let y = 0; y < gridSize; y++) {
     let rowValues = gridList[y];
     let firstColorValue = rowValues[0];
+
     if (firstColorValue > 0) {
+      let noLineRowValues = [true];
       let lineFormed = true;
+
       for (let x = 1; x < gridSize; x++) {
+        noLineRowValues[x] = true;
+
         if (rowValues[x] != firstColorValue) {
           lineFormed = false;
           break
         }
       }
+
       if (lineFormed) {
         winningPlayerID = firstColorValue;
+        noLineGridList[y] = noLineRowValues;
         break;
       }
     }
   }
 
-  return winningPlayerID;
+  return [winningPlayerID, noLineGridList];
 })
 
 // Vertical checking
 
 checkWinConditionFunctions.push(function () {
   let winningPlayerID;
+  let noLineGridList = [];
 
   for (let x = 0; x < gridSize; x++) {
     let firstColorValue = gridList[0][x]
+
     if (firstColorValue > 0) {
+      let noLineRowValues = [];
       let lineFormed = true;
+
+      noLineRowValues[x] = true;
+      noLineGridList[0] = noLineRowValues;
+
       for (let y = 1; y < gridSize; y++) {
+        noLineRowValues = [];
+
+        noLineRowValues[x] = true;
+        noLineGridList[y] = noLineRowValues
+
         if (gridList[y][x] != firstColorValue) {
           lineFormed = false;
           break;
         }
       }
+
       if (lineFormed) {
         winningPlayerID = firstColorValue;
         break;
@@ -312,19 +430,28 @@ checkWinConditionFunctions.push(function () {
     }
   }
 
-  return winningPlayerID;
+  return [winningPlayerID, noLineGridList];
 })
 
 /// Diagonally checking
 
 checkWinConditionFunctions.push(function () {
   let winningPlayerID;
+  let noLineGridList = [];
   let firstColorValue = gridList[0][0];
 
   if (firstColorValue > 0) {
+    let noLineRowValues = [true];
     let lineFormed = true;
 
+    noLineGridList[0] = noLineRowValues;
+
     for (let y = 1; y < gridSize; y++) {
+      noLineRowValues = [];
+
+      noLineRowValues[y] = true;
+      noLineGridList[y] = noLineRowValues;
+
       if (gridList[y][y] != firstColorValue) {
         lineFormed = false;
       }
@@ -339,10 +466,20 @@ checkWinConditionFunctions.push(function () {
     firstColorValue = gridList[0][gridStop];
 
     if (firstColorValue > 0) {
+      let noLineRowValues = [];
       let lineFormed = true;
 
+      noLineRowValues[gridStop] = true;
+      noLineGridList[0] = noLineRowValues;
+
       for (let y = 1; y < gridSize; y++) {
-        if (gridList[y][abs(y - gridStop)] != firstColorValue) {
+        let x = abs(y - gridStop);
+        noLineRowValues = [];
+
+        noLineRowValues[x] = true;
+        noLineGridList[y] = noLineRowValues;
+
+        if (gridList[y][x] != firstColorValue) {
           lineFormed = false;
         }
       }
@@ -353,5 +490,5 @@ checkWinConditionFunctions.push(function () {
     }
   }
 
-  return winningPlayerID;
+  return [winningPlayerID, noLineGridList];
 })
